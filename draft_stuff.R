27daa88cb349b4074c429d80e0f01d2ea5b38a65
pittachalk <>
2018-08-04 08:08:55 +0100

    added some new stuff in the draft after meeting

--- draft_stuff.R
+++ draft_stuff.R
@@ -50,10 +50,32 @@ ggplot(data = diamonds) +
   )
 
 #fancier version
-ggplot(data = diamonds, mapping = aes(x = cut, y = depth)) + 
-  geom_point(alpha = 0.05, position = "jitter") +
+ggplot(data = diamonds, 
+       mapping = aes(x = cut, y = depth)) + 
   stat_summary(fun.ymin = min, fun.ymax = max, 
-               fun.y = median, colour = "red") 
+               fun.y = mean, colour = "red") +
+  geom_point(alpha = 0.5, position = "jitter")
+
+ggplot(data) +
+  geom_point(mapping = aes(x = x, y = y,
+                           colour = marker_gene_status))
+
+ggplot(mapping = aes(x=x, y=y)) +
+  geom_point(data %>% filter(marker == FALSE), 
+             colour = "black") +
+  geom_point(data %>% filter(marker == TRUE), 
+             colour = "red")
+
+data %>% filter(marker == FALSE) %>%
+  group_by(geneid) %>%
+  summarise(mean) -> x
+
+randomVariablewithalOndName <- filter(data, marker == FALSE)
+randomkVariablewithalOndName <- group_by(randomVariablewithalOndName, geneid)
+randomVariablewithalOndName <- sumarrise(randomVariablewithalOndName, mean)
+
+
+
 
 ##############
 
@@ -65,22 +87,25 @@ ggplot(data = diamonds, mapping = aes(x = carat, y = depth) ) +
 ##############
 
 #3.7.1 q5
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = ..prop..,
+                         fill = color))
+
 ggplot(data = diamonds) + 
   geom_bar(mapping = aes(x = cut, y = ..prop..))
-ggplot(data = diamonds) + 
-  geom_bar(mapping = aes(x = cut, fill = color, y = ..prop..))
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, y = ..prop.., group = 1))
 
 ggplot(data = diamonds) +
   geom_bar(mapping = aes(x = cut, y = ..prop.., 
-                         group = 1))
-ggplot(data = diamonds) +
-  geom_bar(mapping = aes(x = cut, fill = color, y = ..prop.., 
-                         group = color))
+                         fill = color, group = color))
 
 #another example (type ?group):
 
 # Multiple groups with one aesthetic
-sample_data <- nlme::Oxboys %>% filter(Subject %in% c(10,19,8,25,22))
+sample_data <- nlme::Oxboys #%>% 
+  #filter(Subject %in% c(10,19,8,25,22))
 
 h <- ggplot(data = sample_data, 
             mapping = aes(age, height, colour = Subject)) + 
@@ -88,8 +113,9 @@ h <- ggplot(data = sample_data,
 h
 
 # this plots
-h + geom_smooth(method = "lm")
-h + geom_smooth(aes(group = Subject), method = "lm")
+h + geom_smooth()
+h + geom_smooth(aes(group = Subject), formula = y~x,
+                method = "lm")
 
 ## fits a single line of best fit across all points
 h + geom_smooth(aes(group = 1), size = 2, method = "lm")
